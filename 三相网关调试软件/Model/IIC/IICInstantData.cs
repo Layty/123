937606edf0044029f6f2ce93a760448e9396306c
dimi1989 <694965217@qq.com>
2020-05-15 11:17:30 +0800

    优化了IIC有无符号数据的处理

--- 三相网关调试软件/Model/IIC/IICInstantData.cs
+++ 三相网关调试软件/Model/IIC/IICInstantData.cs
@@ -73,10 +73,10 @@ namespace 三相智慧能源网关调试软件.Model.IIC
                 var bytes = inputBytes.StringToByte();
                 if (bytes[0] == 0x80 && bytes[1] == 0x01)
                 {
-                    var time = bytes.Skip(2).Take(6).ToArray();
-                    foreach (var VARIABLE in time)
+                    var dateTimeBytes = bytes.Skip(2).Take(6).ToArray();
+                    foreach (var variable in dateTimeBytes)
                     {
-                        DateTime += Convert.ToString(VARIABLE,10).PadLeft(2,'0');
+                        DateTime += Convert.ToString(variable,10).PadLeft(2,'0');
                     }
                     //DateTime = bytes.Skip(2).Take(6).ToArray().ByteToString();
                     Ua = ValueConvertor(bytes.Skip(8), ("V", 3));
@@ -88,25 +88,25 @@ namespace 三相智慧能源网关调试软件.Model.IIC
                     Ic = ValueConvertor(bytes.Skip(28), ("A", 3));
                     In = ValueConvertor(bytes.Skip(32), ("A", 3));
 
-                    P = ValueConvertor(bytes.Skip(36), ("W", 3));
-                    Pa = ValueConvertor(bytes.Skip(40), ("W", 3));
-                    Pb = ValueConvertor(bytes.Skip(44), ("W", 3));
-                    Pc = ValueConvertor(bytes.Skip(48), ("W", 3));
+                    P = ValueConvertor(bytes.Skip(36), ("W", 3),isUnsigned:false);
+                    Pa = ValueConvertor(bytes.Skip(40), ("W", 3), isUnsigned: false);
+                    Pb = ValueConvertor(bytes.Skip(44), ("W", 3), isUnsigned: false);
+                    Pc = ValueConvertor(bytes.Skip(48), ("W", 3), isUnsigned: false);
 
-                    Q = ValueConvertor(bytes.Skip(52), ("var", 3));
-                    Qa = ValueConvertor(bytes.Skip(56), ("var", 3));
-                    Qb = ValueConvertor(bytes.Skip(60), ("var", 3));
-                    Qc = ValueConvertor(bytes.Skip(64), ("var", 3));
+                    Q = ValueConvertor(bytes.Skip(52), ("var", 3), isUnsigned: false);
+                    Qa = ValueConvertor(bytes.Skip(56), ("var", 3), isUnsigned: false);
+                    Qb = ValueConvertor(bytes.Skip(60), ("var", 3), isUnsigned: false);
+                    Qc = ValueConvertor(bytes.Skip(64), ("var", 3), isUnsigned: false);
 
                     S = ValueConvertor(bytes.Skip(68), ("VA", 3));
                     Sa = ValueConvertor(bytes.Skip(72), ("VA", 3));
                     Sb = ValueConvertor(bytes.Skip(76), ("VA", 3));
                     Sc = ValueConvertor(bytes.Skip(80), ("VA", 3));
 
-                     Pf = ValueConvertor(bytes.Skip(84), ("", 4));
-                    Pfa = ValueConvertor(bytes.Skip(88), ("", 4));
-                    Pfb = ValueConvertor(bytes.Skip(92), ("", 4));
-                    Pfc = ValueConvertor(bytes.Skip(96), ("", 4));
+                     Pf = ValueConvertor(bytes.Skip(84), ("", 4), isUnsigned: false);
+                    Pfa = ValueConvertor(bytes.Skip(88), ("", 4), isUnsigned: false);
+                    Pfb = ValueConvertor(bytes.Skip(92), ("", 4), isUnsigned: false);
+                    Pfc = ValueConvertor(bytes.Skip(96), ("", 4), isUnsigned: false);
 
                     angle_Ua = ValueConvertor(bytes.Skip(100), ("", 3));
                     angle_Ub = ValueConvertor(bytes.Skip(104), ("", 3));
@@ -120,11 +120,15 @@ namespace 三相智慧能源网关调试软件.Model.IIC
                     PosReactT = ValueConvertor(bytes.Skip(132), ("var", 3));
                     RevReactT = ValueConvertor(bytes.Skip(136), ("var", 3));
 
-                    RtcBattV = ValueConvertor(bytes.Skip(140),("v",2));
+                    RtcBattV = ValueConvertor(bytes.Skip(140),("V",2));
 
                     Freq = ValueConvertor(bytes.Skip(144), ("Hz",2),2);
                     CurRate = ValueConvertor(bytes.Skip(146), ("",0),1);
                     RunMode = ValueConvertor(bytes.Skip(147), ("",0),1);
+                    ErrStatus = ValueConvertor(bytes.Skip(148), ("", 0), 2);
+                    StealStatus = ValueConvertor(bytes.Skip(150), ("", 0), 4);
+                    EMUStatus = ValueConvertor(bytes.Skip(154), ("", 0), 4);
+                    RunStatus = ValueConvertor(bytes.Skip(158), ("", 0), 2);
                     return true;
                 }
                 else
@@ -142,29 +146,65 @@ namespace 三相智慧能源网关调试软件.Model.IIC
         }
 
         public string RunMode { get; set; }
-
+        public string ErrStatus { get; private set; }
+        public string StealStatus { get; private set; }
+        public string EMUStatus { get; private set; }
+        public string RunStatus { get; private set; }
         public string CurRate { get; set; }
 
         public string Freq { get; set; }
 
         private string ValueConvertor(IEnumerable<byte> dataBytes, (string Unit, int Scalar) unitAndScalar,
-            int length = 4)
+            int length = 4, bool isUnsigned = true)
         {
-            uint result = 0;
+            dynamic result=0 ;
             string resultstring = "";
             if (length == 4)
             {
-                result = BitConverter.ToUInt32(dataBytes.Take(length).Reverse().ToArray(), 0);
+                if (isUnsigned)
+                {
+                    result = BitConverter.ToUInt32(dataBytes.Take(length).Reverse().ToArray(), 0);
+                }
+                else
+                {
+                    result = BitConverter.ToInt32(dataBytes.Take(length).Reverse().ToArray(), 0);
+                }
+               
             }
             else if (length == 2)
             {
-                result = BitConverter.ToUInt16(dataBytes.Take(length).Reverse().ToArray(), 0);
+                if (isUnsigned)
+                {
+                    result = BitConverter.ToUInt16(dataBytes.Take(length).Reverse().ToArray(), 0);
+                }
+                else
+                {
+                    result = BitConverter.ToInt16(dataBytes.Take(length).Reverse().ToArray(), 0);
+                }
+              
             }
             else if (length == 1)
             {
-                result = dataBytes.Take(length).ToArray()[0];
+                if (isUnsigned)
+                {
+                    result =Convert.ToByte(dataBytes.Take(length).ToArray()[0]) ;
+                }
+                else
+                {
+                    result = Convert.ToSByte(dataBytes.Take(length).ToArray()[0]);
+                }
+               
             }
 
+            resultstring= HandlerUnitScalar(result, (unitAndScalar));
+
+            return resultstring;
+            // return (result*0.001).ToString();
+        }
+
+        private string HandlerUnitScalar(dynamic result, (string Unit, int Scalar) unitAndScalar)
+        {
+            string resultstring= result.ToString();
             if (unitAndScalar.Scalar == 0)
             {
                 resultstring = (result).ToString() + unitAndScalar.Unit;
@@ -187,7 +227,6 @@ namespace 三相智慧能源网关调试软件.Model.IIC
             }
 
             return resultstring;
-            // return (result*0.001).ToString();
         }
     }
 }
