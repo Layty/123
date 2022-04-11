dc1caacd940cdae9976fa48a746460e87d4cae1f
dimi1989 <694965217@qq.com>
2022-04-11 08:45:50 +0800

    1、加入缺少的Release代码类

---
+++ MyDlmsStandard/ApplicationLay/Release/ReleaseRequest.cs
@@ -0,0 +1,75 @@
+﻿using MyDlmsStandard.ApplicationLay.ApplicationLayEnums;
+using MyDlmsStandard.ApplicationLay.Association;
+using MyDlmsStandard.ApplicationLay.Get;
+using MyDlmsStandard.Ber;
+using MyDlmsStandard.Common;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace MyDlmsStandard.ApplicationLay.Release
+{
+    public class ReleaseRequest : IDlmsCommand, IToPduStringInHex, IPduStringInHexConstructor
+    {
+        [XmlIgnore] public Command Command { get; } = Command.ReleaseRequest;
+        public BerInteger Reason { get; set; }
+
+        public UserInformation UserInformation { get; set; }
+
+
+
+        public string ToPduStringInHex()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            if (Reason != null)
+            {
+                stringBuilder.Append("80");
+                stringBuilder.Append(Reason.ToPduStringInHex());
+            }
+
+            if (UserInformation != null)
+            {
+                string str = UserInformation.ToPduBytes().ByteToString();
+                stringBuilder.Append(str);
+            }
+
+            return "62" + (stringBuilder.Length / 2).ToString("X2") + stringBuilder.ToString();
+        }
+        public bool PduStringInHexConstructor(ref string pduStringInHex)
+        {
+            if (string.IsNullOrEmpty(pduStringInHex))
+            {
+                return false;
+            }
+            string command = pduStringInHex.Substring(0, 2);
+            if (command != "62")
+            {
+                return false;
+            }
+            return true;
+        }
+
+        //        public void TransferInitiateReqToUiClean()
+        //        {
+        //            if (InitiateRequest != null)
+        //            {
+        //                TransferInitiateReqToUi(InitiateRequest.ToPduStringInHex());
+        //            }
+        //        }
+
+        //        public void TransferInitiateReqToUiCipher(string irTag, string cipherText)
+        //        {
+        //            BerOctetString berOctetString = new BerOctetString();
+        //            berOctetString.Value = cipherText;
+        //            TransferInitiateReqToUi(irTag + berOctetString.ToPduStringInHex());
+        //        }
+
+        //        private void TransferInitiateReqToUi(string irText)
+        //        {
+        //            BerOctetString berOctetString = new BerOctetString();
+        //            berOctetString.Value = irText;
+        //            string str = berOctetString.ToPduStringInHex();
+        //            UserInformation = new BerOctetString();
+        //            UserInformation.Value = "04" + str;
+        //        }
+    }
+}
---
+++ MyDlmsStandard/ApplicationLay/Release/ReleaseResponse.cs
@@ -0,0 +1,84 @@
+﻿using MyDlmsStandard.ApplicationLay.ApplicationLayEnums;
+using MyDlmsStandard.ApplicationLay.Association;
+using MyDlmsStandard.ApplicationLay.Get;
+using MyDlmsStandard.Ber;
+using System;
+using System.Xml.Serialization;
+
+namespace MyDlmsStandard.ApplicationLay.Release
+{
+    public class ReleaseResponse : IDlmsCommand, IPduStringInHexConstructor
+    {//00 01 00 01 00 01 00 05 63 03 80 01 00
+     //		<ReleaseResponse>
+     //  <Reason Value = "Normal" />
+     //</ ReleaseResponse >
+
+        [XmlIgnore] public Command Command { get; } = Command.ReleaseResponse;
+        public BerInteger Reason { get; set; }
+        public InitiateResponse UserInformation { get; set; }
+
+
+        public bool PduStringInHexConstructor(ref string pduStringInHex)
+        {
+            if (string.IsNullOrEmpty(pduStringInHex))
+            {
+                return false;
+            }
+
+            string command = pduStringInHex.Substring(0, 2);
+            if (command != "63")
+            {
+                return false;
+            }
+            int num = Convert.ToInt32(pduStringInHex.Substring(2, 2), 16);
+            if (num * 2 + 2 > pduStringInHex.Length)
+            {
+                return false;
+            }
+            string pduStringInHex2 = pduStringInHex.Substring(4, num * 2);
+            pduStringInHex = pduStringInHex.Substring(4 + num * 2);
+            while (pduStringInHex2.Length > 0)
+            {
+                switch (Convert.ToInt32(pduStringInHex2.Substring(0, 2), 16) & 0x1F)
+                {
+                    case 0:
+                        pduStringInHex2 = pduStringInHex2.Substring(2);
+                        Reason = new BerInteger();
+                        if (!Reason.PduStringInHexConstructor(ref pduStringInHex2))
+                        {
+                            return false;
+                        }
+                        break;
+                    case 30:
+                        {
+                            pduStringInHex2 = pduStringInHex2.Substring(2);
+                            BerOctetString berOctetString = new BerOctetString();
+                            if (!berOctetString.PduStringInHexConstructor(ref pduStringInHex2))
+                            {
+                                return false;
+                            }
+                            if (berOctetString.Value.StartsWith("04"))
+                            {
+                                string pduStringInHex3 = berOctetString.Value.Substring(2);
+                                berOctetString = new BerOctetString();
+                                if (!berOctetString.PduStringInHexConstructor(ref pduStringInHex3))
+                                {
+                                    return false;
+                                }
+                                pduStringInHex3 = berOctetString.Value;
+                                UserInformation = new InitiateResponse();
+                                if (!UserInformation.PduStringInHexConstructor(ref pduStringInHex3))
+                                {
+                                    return false;
+                                }
+                                break;
+                            }
+                            return false;
+                        }
+                }
+            }
+            return true;
+
+        }
+    }
+}
